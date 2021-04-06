caa1e485893de8dbe2165603e9c335186479d347
Lucía Muñoz González <luciamgonzalez1997@gmail.com>
2021-04-06 15:08:56 +0100

    Añadida la funcionalidad del formulario

--- src/index.js
+++ src/index.js
@@ -43,3 +43,92 @@
 
   initSlider();
 })();
+
+/* Formulario */
+const formcontact = document.getElementById("form");
+const inputs = document.querySelectorAll("#form input"); // nos trae un arreglo con todos los inputs
+const textarea = document.querySelectorAll("#form textarea");
+// objeto
+const
+  expression = {
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    textarea: /^[^$%&|<>#]*$/, // Letras y espacios, pueden llevar acentos.
+    phone: /^\d{7,14}$/ // 7 a 14 numeros.
+  };
+
+const
+  fields = {
+    name: false,
+    email: false,
+    phone: false,
+    textarea: false,
+  };
+
+// Se comprueba que cuando clickas en el input se escribe lo que tengamos en el atributo name
+const validateform = (e) => {
+  // Se ejecuta cuando el atributo name le corresponde input name
+  switch (e.target.name) {
+    case "name":
+      // Se comprueba que el valor del input se corresponda con la expresion regular
+      validatefield(expression.name, e.target, "name");
+      break;
+    case "email":
+      validatefield(expression.email, e.target, "email");
+      break;
+    case "phone":
+      validatefield(expression.phone, e.target, "phone");
+      break;
+    case "textarea":
+      validatefield(expression.textarea, e.target, "textarea");
+      break;
+  }
+};
+
+// para acceder a la expresion del input nombre o email o etc hay que pasarle 3 parámetros
+const validatefield = (expression, input, field) => {
+  if (expression.test(input.value)) {
+    document.getElementById(`group__${field}`).classList.remove("form__group-incorrect");
+    document.getElementById(`group__${field}`).classList.add("form__group-correct");
+    document.querySelector(`#group__${field} i`).classList.add("fa-check-circle");
+    document.querySelector(`#group__${field} i`).classList.remove("fa-times-circle");
+    document.querySelector(`#group__${field} .form__input-error`).classList.remove("form__input-error-active");
+    // se conprueban que los datos están todos correctos para enviar
+
+    fields[field] = "true";
+  } else {
+    document.getElementById(`group__${field}`).classList.add("form__group-incorrect");
+    document.getElementById(`group__${field}`).classList.remove("form__group-correct");
+    document.querySelector(`#group__${field} i`).classList.add("fa-times-circle");
+    document.querySelector(`#group__${field} i`).classList.remove("fa-check-circle");
+    document.querySelector(`#group__${field} .form__input-error`).classList.add("form__input-error-active");
+
+    fields[field] = "false";
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validateform);
+  input.addEventListener("blur", validateform);
+  // Al levantar y presionar se comprueba que se validan los campos tanto en el input como fuera de él---
+});
+
+textarea.forEach((textarea) => {
+  textarea.addEventListener("keyup", validateform);
+  textarea.addEventListener("blur", validateform);
+  // Al levantar y presionar se comprueba que se validan los campos tanto en el input como fuera de él---
+});
+
+formcontact.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (fields.name && fields.email && fields.phone && fields.textarea) {
+    formcontact.reset();
+
+    document.getElementById("form__message-correct").classList.add("form__message-correct-active");
+    setTimeout(() => {
+      document.getElementById("form__message-correct").classList.remove("formulario__message-correct-active");
+    }, 5000);
+  } else {
+    document.getElementById("form__message").classList.add("form__message-active");
+  }
+});
