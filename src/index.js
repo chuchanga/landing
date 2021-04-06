ffb3f95238ec07aaba1aa88f106223b6add87b53
Lucía Muñoz González <luciamgonzalez1997@gmail.com>
2021-04-06 15:42:06 +0100

    Añadida animación scroll

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 (function() {
   const sliderImages = document.querySelectorAll(".slider-item");
   const arrowLeft = document.querySelector("#left-arrow");
@@ -42,6 +43,40 @@
   });
 
   initSlider();
+})();
+
+(function() {
+  let elements = [];
+  let windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll(".hidden");
+    elements = Array.prototype.slice.call(elements);
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      const positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add("fade-in-element");
+        element.classList.remove("hidden");
+
+        elements = elements.filter((item) => element != item);
+        if (elements.length <= 0) {
+          window.removeEventListener("scroll", checkPosition);
+          window.removeEventListener("resize", init);
+        }
+      }
+    }
+  }
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
 })();
 
 /* Formulario */
